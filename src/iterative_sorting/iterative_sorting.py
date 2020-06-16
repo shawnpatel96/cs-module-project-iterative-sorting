4a20023ea61f0447d2e8352d9e7d1708cf5accb3
shawn <shawnpatel196@gmail.com>
2020-06-15 20:30:40 -0500

    finished mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,17 +7,30 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for x in range(cur_index + 1, len(arr)):
+            if arr[smallest_index] > arr[x]:
+                smallest_index = x
 
         # TO-DO: swap
         # Your code here
-
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    #need to loop through
+    #compare
+    # if smaller than right swap positions
+    # rinse and repeat till reaching the end of array
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(arr)- 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swapped = True
 
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,29 @@
 def linear_search(arr, target):
     # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
+    return -1  # not found
 
-    return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
+    #set index 0 and last index
+    left = 0
+    right = len(arr) - 1  #dynamic setting of last index, array could be million elements, still will be the last one
     # Your code here
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+
+        if arr[mid] < target:
+            left = mid  + 1 
+        else:
+            right = mid - 1
+
 
 
     return -1  # not found
